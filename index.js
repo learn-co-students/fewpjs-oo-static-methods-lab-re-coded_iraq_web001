b550d2eabe483cfa4b9f87677c80de59106a61c6
Abdulrahman Maher <ibn.maher.96@gmail.com>
2020-06-14 11:07:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string[0].toUpperCase() + string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^\w'-\s]/gi, '')
+  }
+  static titleize(string){
+    let arr = string.split(' ');
+    let newArr = []
+    newArr.push(arr[0].charAt(0).toUpperCase() + arr[0].slice(1))
+    for(let i=1;i<arr.length;i++){
+      if( arr[i] === "the" ||arr[i] ==="an"||arr[i] ==="of"||arr[i] ==="a"||arr[i] ==="for"||arr[i] ==="and"||arr[i] ==="at"||arr[i] ==="by"||arr[i] ==="from"||arr[i] ==="but" ){
+newArr.push(arr[i])
+      }
+      else{
+        newArr.push(arr[i].charAt(0).toUpperCase() + arr[i].slice(1))
+      }
+    }
+    return newArr.join(" ")
+
+  }
 }
