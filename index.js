c99a51e8beecd0a3573d764936f2f15697ee7571
Arvin  Alhissou <arvin.alhissou@gmail.com>
2020-06-14 23:59:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+     return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+  static sanitize (str){
+  return str.replace(/[^A-Za-z0-9-" "']+/g, '')
+}
+  static titleize(str){
+   const exceptions = ['the','a', 'an', 'but', 'of', 'and', 'for', 'at','by','from']
+
+    let myStr = str.split(" ");
+    let arr = [];
+
+    myStr.map(word=>{
+      if(exceptions.includes(word) === false){
+        arr.push(Formatter.capitalize(word));
+      } else {
+        arr.push(word);
+      }
+    })
+    return Formatter.capitalize(arr.join(" "));
+  }
 }
