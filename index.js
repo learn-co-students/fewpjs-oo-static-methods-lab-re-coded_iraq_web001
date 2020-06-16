22a6eb633d45ca91a22e31ef234741fe4c35d67d
Bawan Majid <bawanmajid23@gmail.com>
2020-06-16 13:57:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+  
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+  
+  static titleize(str){
+    let word = str.split(' ');
+    let except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    
+    word[0] = this.capitalize(word[0]);
+    for (let w = 1; w < word.length; w++) {
+      if(!except.find(nonWord => nonWord == word[w])){
+         word[w] = this.capitalize(word[w]);
+      }
+    }
+    
+    return word.join(' ');
+}
 }
