450d42c29aaeb8d0fdfbdcfc0c3fe63977382aba
Hewr Srood <hewr_s@outlook.com>
2020-06-22 09:16:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 class Formatter {
-  //add static methods here
+  static  capitalize(str){
+     returns str.replace(/^\w/, (c) => c.toUpperCase());
+
+  }
+  static sanitize (str){
+      newStr=str.split();
+      newStr.filter((str)=>{
+        if(str.match(/^[A-Za-z-'--]+$/))
+      })
+  }
 }
