7bed5f6e60bb44a5a2fcf4a66a6193de0dbba15b
Hussein Talal <hossientalal2@gmail.com>
2020-06-22 08:38:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-class Formatter {
-  //add static methods here
-}
+  class Formatter {
+    //add static methods here
+    static capitalize(word){
+      return word[0].toUpperCase() + word.split("").slice(1).join("")
+    }
+    static sanitize(word){
+      return word.replace(/[^'\w\s-]/g,"")
+    }
+    static titleize(string){
+      let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+      return (string.charAt(0).toUpperCase() + string.split(' ').map(word => exceptions.includes(word) ? word :word[0].toUpperCase() + word.split("").slice(1).join("") ).join(" ").slice(1))
+    }
+
+  }
