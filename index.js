080a2abab12a1b3004c2b408131f83ecb8888cc6
Revan Sarbast <revansarbast8@gmail.com>
2020-07-03 09:07:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string[0].toUpperCase()+string.slice(1,string.length);
+  }
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9- ']+/g, '')
+  }
+  static titleize(str){
+    let data = str.split(' ')
+    data[0] = this.capitalize(data[0])
+    for(let i = 0; i < data.length; i++){
+      switch(data[i]){
+        case 'the':
+          break;
+        case 'a':
+          break;
+        case 'an':
+          break;
+        case 'from':
+          break;
+        case 'but':
+          break;
+        case 'of':
+          break;
+        case 'and':
+          break;
+        case 'at':
+          break;
+        case 'by':
+          break;
+        case 'for':
+          break;
+        default:
+          data[i] = this.capitalize(data[i])
+          break;
+        
+      }
+    }
+    str = ''
+    for(let i = 0; i < data.length-1; i++){
+      str += data[i]+' '
+    }
+    return str+data[data.length-1];
+  }
 }
+console.log(Formatter.titleize('dkhfdklhd dhsffh the'))
